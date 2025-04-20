ea8eea7a6e972a5a140ec28a4235c342f1f56908
AnasKhan667 <anas493484@gmail.com>
2025-04-20 15:46:54 +0500

    Initial commit

--- store/serializers.py
+++ store/serializers.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['product_name', 'slug', 'description', 'price', 'stock']
